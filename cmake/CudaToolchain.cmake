21300e30cc247037d37888dac58fa4b3b541c84a
alexdon256 <oleksandr.don.256@gmail.com>
2025-11-21 03:24:49 +0200

    Add CMake CUDA toolchain file (force add)

---
+++ cmake/CudaToolchain.cmake
@@ -0,0 +1,18 @@
+# CMake Toolchain file for CUDA with unsupported compiler support
+# This file is loaded before project() call, allowing flags to be set early
+
+# Set CUDA flags for unsupported compiler versions (needed for newer VS2022 with CUDA 11.8)
+# These must be set as initial values before project() processes CUDA language
+if(NOT DEFINED CMAKE_CUDA_FLAGS_INIT)
+    set(CMAKE_CUDA_FLAGS_INIT "-allow-unsupported-compiler" CACHE STRING "Initial CUDA compiler flags" FORCE)
+elseif(NOT CMAKE_CUDA_FLAGS_INIT MATCHES "-allow-unsupported-compiler")
+    set(CMAKE_CUDA_FLAGS_INIT "${CMAKE_CUDA_FLAGS_INIT} -allow-unsupported-compiler" CACHE STRING "Initial CUDA compiler flags" FORCE)
+endif()
+
+# Also set for regular compilation
+if(NOT DEFINED CMAKE_CUDA_FLAGS)
+    set(CMAKE_CUDA_FLAGS "-allow-unsupported-compiler" CACHE STRING "CUDA compiler flags" FORCE)
+elseif(NOT CMAKE_CUDA_FLAGS MATCHES "-allow-unsupported-compiler")
+    set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} -allow-unsupported-compiler" CACHE STRING "CUDA compiler flags" FORCE)
+endif()
+
